0a8852f253ba8a34db1b5c0400b6783c32378c38
Olga Gorblyanska <olga.gorblyanska@email.cz>
2019-02-20 09:03:46 +0100

    zmena

---
+++ assets/app.js
@@ -0,0 +1,31 @@
+let poleOsob = [{
+        jmeno: 'Jurij',
+        prijmeni: 'Gagarin',
+        datumnarození: '9.5.1934',
+        superschopnost: 'První člověk, který vzlétl do vesmíru.',
+    },
+    {
+        jmeno: 'John Herschel',
+        prijmeni: 'Glenn',
+        datumnarození: '18.7.1921',
+        superschopnost: 'Trikrát obletel Zem',
+    },
+    {
+        jmeno: 'Alan',
+        prijmeni: 'Shepard',
+        datumnarození: '18.10.1923',
+        superschopnost: 'Nejstarší člověk, který vstoupil na měsic',
+    },
+    {
+        jmeno: 'Valentina',
+        prijmeni: 'Tereškovová',
+        datumnarození: '6.5.1937',
+        superschopnost: 'prvni žena na ceste do vesmíru',
+    },
+    {
+        jmeno: 'Vladimír',
+        prijmeni: 'Remek',
+        datumnarození: '26.9.1948',
+        superschopnost: 'První cizinec, který se zúčastnil mezinárodních pilotovaných vesmírných letů programu Interkosmos',
+    }
+]
